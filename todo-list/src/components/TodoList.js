c05adc2de6a7c481f62de0f62bcb8f85cd8d86ca
nitin-dhevar <nitin.dhanabalan@gmail.com>
2024-05-28 00:13:38 -0400

    Todo list app components added

---
+++ todo-list/src/components/TodoList.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+
+function TodoList() {
+    const [tasks, setTasks] = useState([
+    {
+    id: 1,
+    text: 'Complete Assignments',
+    completed: true
+    },
+    {
+    id: 2,
+    text: 'Go to gym',
+    completed: false
+    }
+    ]);
+    
+    const [text, setText] = useState('');
+   function addTask(text) {
+    const newTask = {
+    id: Date.now(),
+    text,
+    completed: false
+    };
+    setTasks([...tasks, newTask]);
+    setText('');
+    }
+   function deleteTask(id) {
+    setTasks(tasks.filter(task => task.id !== id));
+    }
+   function toggleCompleted(id) {
+    setTasks(tasks.map(task => {
+    if (task.id === id) {
+    return {...task, completed: !task.completed};
+    } else {
+    return task;
+    } 
+    }));
+    }
+   return (
+    <div className="todo-list">
+    {tasks.map(task => (
+    <TodoItem
+    key={task.id} 
+    task={task}
+    deleteTask={deleteTask}
+    toggleCompleted={toggleCompleted} 
+    />
+    ))}
+   <input
+    value={text}
+    onChange={e => setText(e.target.value)} 
+    />
+   <button onClick={() => addTask(text)}>Add</button>
+    </div>
+    );
+   }
+   export default TodoList;
+   
